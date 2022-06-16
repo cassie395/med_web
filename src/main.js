f76c62b98f4b8b145e51aa261191fafa788c9fc5
yusin6400 <64146523+yusin6400@users.noreply.github.com>
2022-06-16 14:40:43 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,22 +3,27 @@ import App from './App.vue'
 import vuetify from './plugins/vuetify'
 import router from './router'
 
+import axios from "axios"
+
 Vue.config.productionTip = false
+Vue.prototype.axios = axios
+axios.defaults.baseURL = 'http://127.0.0.1'
 
 new Vue({
   vuetify,
   router,
+  // store,
   render: h => h(App)
 }).$mount('#app')
 
-router.beforeEach((to, from, next)=>{
-  const isLogin = localStorage.getItem('token') == 'ImLogin' ;
-  if( isLogin ){
-    next();
-  } else {
-    if( to.path !== '/')
-      next('/');
-    else
-      next();
-  }
-});
+// router.beforeEach((to, from, next)=>{
+//   // const isLogin = localStorage.getItem('token') == 'ImLogin' ;
+//   if( this.user.uid!='' ){ //isLogin
+//     next();
+//   } else {
+//     if( to.path !== '/')
+//       next('/');
+//     else
+//       next();
+//   }
+// });
