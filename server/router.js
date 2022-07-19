f6897b78f34196276f690e60303f9ed936efb573
yusin6400 <64146523+yusin6400@users.noreply.github.com>
2022-07-19 16:34:53 +0800

    Update router.js

--- server/router.js
+++ server/router.js
@@ -1,11 +1,11 @@
 let express=require('express')
 let router=express.Router()
 
-// let user=require('./API/user')
+let user=require('./API/user')
 let login=require('./API/login')
 let MedNum=require('./API/MedNum')
 
-// router.get('/user', user.get)
+router.get('/user', user.get)
 router.get('/', login.login)
 router.get('/getMedNum', MedNum.getMedNum)
 router.get('/updateMedNum', MedNum.updateMedNum)
