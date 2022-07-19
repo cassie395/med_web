0338757fd3b45c14b7960fcf0b885784604104c6
yusin6400 <64146523+yusin6400@users.noreply.github.com>
2022-07-19 16:35:34 +0800

    Update user.js

--- server/API/user.js
+++ server/API/user.js
@@ -1,11 +1,14 @@
 let db=require('../db/index')
 
 exports.get=(req, res) => {
-    var sql = 'select * from user'
-    db.query(sql, [req.query.name, req.query.password], (err, data) => {
+    var sql = 'select * from user where uid = ?'
+    db.query(sql, [req.query.uid], (err, data) => {
+        console.log('user: '+ data[0].uname);
         if(err){
             return res.send('error: '+ err.message)
         }
-        res.send(data)
+        if(data.length>0){
+            res.json(data);
+        }
     })
 }
